5bd79f03a2bbc8adca72b8ac20aa921804a4ea40
DiegoFHG <34254918+DiegoFHG@users.noreply.github.com>
2020-04-27 20:08:32 +0200

    Finished cosine similarity implementation.

--- src/main/java/com/kebabwarrios/es/Searcher.java
+++ src/main/java/com/kebabwarrios/es/Searcher.java
@@ -1,7 +1,9 @@
 package com.kebabwarrios.es;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public final class Searcher {
   public static HashMap<String, Double> search(
@@ -10,37 +12,63 @@ public final class Searcher {
     HashMap<String, HashMap<String[], Double[]>> indexedDocuments
   ) {
     HashMap<String, Double> vectorQuery = tdIdfVector(query, documents);
-    HashMap<String, Double> vectorQuerySquared = new HashMap<>();
-
-    Double numerator;
-    Double denominator;
-
-    vectorQuery.forEach((keyOne, valueOne) -> {
-      vectorQuerySquared.put(keyOne, Math.pow(valueOne, 2));
-    });
+    HashMap<String, Double> result = new HashMap<>();
     
     indexedDocuments.forEach((keyOne, valueOne) -> {
-      ArrayList<Double> documentVectorSquared = new ArrayList<>();
-      
       valueOne.forEach((keyTwo, valueTwo) -> {
-        for (int x = 0; x < valueTwo.length; x++)
-          documentVectorSquared.add(Math.pow(valueTwo[x], 2));
-      });
-      
-      vectorQuerySquared.forEach((keyThree, valueThree) -> {
-        documentVectorSquared.forEach((value) -> {
-          numerator.add(valueThree * value);
-        });
+        HashMap<String, Double> word = new HashMap<>();
+
+        for (int x = 0; x < valueOne.size() ; x++) {
+          word.put(keyTwo[x], valueTwo[x]);
+        }
+
+        result.put(keyOne, (makeNumerator(vectorQuery, word) / makeDenominator(vectorQuery, word)));
       });
     });
+    
+    return result;
   }
   
-  private static HashMap<String, Double> makeDenominator(
+  private static Double makeNumerator(
+    HashMap<String, Double> vectorOne,
+    HashMap<String, Double> vectorTwo
+  ) {
+    ArrayList<Double> vectorOneList = new ArrayList<>();
+    ArrayList<Double> vectorTwoList = new ArrayList<>();
+    Double[] vectorOneArray;
+    Double[] vectorTwoArray;
+    ArrayList<Double> vectorMultiplication = new ArrayList<>();
+    
+    vectorOne.forEach((key, value) -> vectorOneList.add(value));
+    vectorTwo.forEach((key, value) -> vectorTwoList.add(value));
+    
+    vectorOneArray = vectorOneList.toArray(new Double[vectorOneList.size()]);
+    vectorTwoArray = vectorTwoList.toArray(new Double[vectorTwoList.size()]);
+    
+    if (vectorOneArray.length >= vectorTwoArray.length) {
+      for (int x = 0; x < vectorOneArray.length; x++) {
+        vectorMultiplication.add(vectorOneArray[x] * vectorTwoArray[x % vectorOneArray.length]);
+      }
+    } else {
+      for (int x = 0; x < vectorTwoArray.length; x++) {
+        vectorMultiplication.add(vectorTwoArray[x] * vectorOneArray[x % vectorTwoArray.length]);
+      }
+    }
+    
+    return vectorMultiplication.stream().mapToDouble(x -> x).sum();
+  }
+  
+  private static Double makeDenominator(
     HashMap<String, Double> vectorOne,
     HashMap<String, Double> vectorTwo
   ) {
     HashMap<String, Double> vectorOneSquared = new HashMap<String, Double>();
     HashMap<String, Double> vectorTwoSquared = new HashMap<String, Double>();
+    ArrayList<Double> vectorOneSquaredList = new ArrayList<>();
+    ArrayList<Double> vectorTwoSquaredList = new ArrayList<>();
+
+    Double vectorOneSquaredSum;
+    Double vectorTwoSquaredSum;
     
     vectorOne.forEach((key, value) -> {
       vectorOneSquared.put(key, Math.pow(value, 2));
@@ -49,6 +77,19 @@ public final class Searcher {
     vectorTwo.forEach((key, value) -> {
       vectorTwoSquared.put(key, Math.pow(value, 2));
     });
+    
+    vectorOneSquared.forEach((key, value) -> {
+      vectorOneSquaredList.add(value);
+    });
+    
+    vectorTwoSquared.forEach((key, value) -> {
+      vectorTwoSquaredList.add(value);
+    });
+    
+    vectorOneSquaredSum = vectorOneSquaredList.stream().mapToDouble(x -> x).sum();
+    vectorTwoSquaredSum = vectorTwoSquaredList.stream().mapToDouble(x -> x).sum();
+    
+    return (Math.sqrt(vectorOneSquaredSum) * Math.sqrt(vectorTwoSquaredSum));
   }
   
   private static String[] vectorize(String query) {
