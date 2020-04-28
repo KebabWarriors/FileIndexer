7ab81cc2ff31626af6d3641ffb46c6970f440acf
DiegoFHG <34254918+DiegoFHG@users.noreply.github.com>
2020-04-28 23:48:44 +0200

    Created py4j gateway.

--- src/main/java/com/kebabwarriors/file_indexer/App.java
+++ src/main/java/com/kebabwarriors/file_indexer/App.java
@@ -7,20 +7,17 @@ import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
+import py4j.GatewayServer;
+
 public class App 
 {
   public static void main( String[] args ) throws FileNotFoundException, IOException
   {
     java.util.logging.Logger
       .getLogger("org.apache.fontbox").setLevel(java.util.logging.Level.SEVERE);    
-
-    Reader reader = new Reader();
-    reader.readDocumentsPath("src/documents");
-
-    FileIndexer fileIndexer = new FileIndexer(reader.getAllDocuments());
-
-    HashMap<String, Double> lol = SearchEngine.search("El Libro de Abraham es una obra de 1835 producida por el fundador del Movimiento de los Santos de los Últimos Días, Joseph Smith, que según él se basó", fileIndexer.getDocumentsMap(), fileIndexer.getIndexedDocumentsMap());
     
-    lol.forEach((key, value) -> System.out.println("Documento: " + key + ", coincidencia: " + value));
+    GatewayServer gatewayServer = new GatewayServer(null);
+    gatewayServer.start();
+    System.out.println("Gateway started at: " + gatewayServer.getPort());
   }
 }
