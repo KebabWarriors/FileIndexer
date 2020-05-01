c62fbcaf64f4b854f11be0d0c585f70bc72e30f5
DiegoFHG <34254918+DiegoFHG@users.noreply.github.com>
2020-05-01 17:54:02 +0200

    Ported project to Java 14 on J9 JVM and ZGC garbage collector.

---
+++ src/main/java/com/kebabwarriors/fileindexer/DocumentReader.java
@@ -0,0 +1,241 @@
+package com.kebabwarriors.fileindexer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+public class DocumentReader {
+  private ArrayList<String[]> documents = new ArrayList<String[]>();
+
+  /**
+   * 
+   * @param folder
+   * @throws IOException 
+   * @throws FileNotFoundException 
+   */
+  public void readDocumentsPath(String path) throws FileNotFoundException, IOException {
+    final File folder = new File(path);
+    
+    Main.logger.debug("Reading documents from path " + path + ".");
+    
+    for (final File fileEntry : folder.listFiles()) {
+      if (fileEntry.isDirectory()) {
+        Main.logger.trace(
+            "File " + fileEntry.getName() + " at " + fileEntry.getAbsolutePath() + " is a directory.");
+        
+        readDocumentsPath(fileEntry);
+      } else {
+        Main.logger.trace(
+            "File " + fileEntry.getName() + " at path " + fileEntry.getPath() + "is a document, adding to list.");
+
+        this.addDocumentToArray(new String[] {
+            fileEntry.getPath(),
+            fileEntry.getName(),
+            this.getFileExtension(fileEntry),
+            this.getGeneralDocumentsContent(fileEntry.getPath(), this.getFileExtension(fileEntry))
+        });
+      }
+    }
+  }
+
+  /*
+   * Override with different parameter.
+   */
+  public void readDocumentsPath(File folder) throws FileNotFoundException, IOException {
+    Main.logger.debug(
+        "Reading documents from directory " + folder.getName() + " at path " + folder.getAbsolutePath() + ".");
+
+    for (final File fileEntry : folder.listFiles())
+      if (fileEntry.isDirectory()) {
+        Main.logger.trace(
+            fileEntry.getName() + " at " + fileEntry.getAbsolutePath() + " is a directory.");
+
+        readDocumentsPath(fileEntry);
+      } else {
+        Main.logger.trace(
+            fileEntry.getName() + " at " + fileEntry.getAbsolutePath() + " is a document, adding to list.");
+
+        this.addDocumentToArray(new String[]{
+            fileEntry.getPath(),
+            fileEntry.getName(),
+            this.getFileExtension(fileEntry),
+            this.getGeneralDocumentsContent(fileEntry.getPath(), this.getFileExtension(fileEntry))
+        });
+      }
+  }
+
+  /**
+   * 
+   * @param path
+   * @param extension
+   * @throws FileNotFoundException
+   * @throws IOException
+   */
+  public String getGeneralDocumentsContent(String path, String extension) throws FileNotFoundException, IOException {
+    String content = "";
+    
+    Main.logger.debug("Reading document at " + path + " with the extension " + extension + ".");
+
+    switch(extension) {
+    case ".docx":
+      content = this.getDocxTextContent(path);
+
+      break;
+    case ".txt":
+      content = this.getPlainTextContent(path);
+
+      break;
+    case ".pdf":
+      content = this.getPDFContent(path);
+
+      break;
+    default:
+      Main.logger.debug("Document's extension is not supported. Skipping.");
+      break;
+    }
+    return content;
+  }
+
+  /**
+   * 
+   * @param path
+   */
+  public void addDocumentToArray(String[] documents) {
+    this.documents.add(documents);
+  }
+
+  /**
+   * 
+   * @return
+   */
+  public ArrayList<String[]> getAllDocuments() {
+    return this.documents;
+  }
+
+  /**
+   * 
+   * @param file
+   * @return
+   */
+  private String getFileExtension(File file) {
+    String name = file.getName();
+    int lastIndexOf = name.lastIndexOf(".");
+
+    if (lastIndexOf == (-1))
+      return ""; // empty extension
+
+    return name.substring(lastIndexOf);
+  }
+
+  /**
+   * 
+   * @param path
+   * @return
+   * @throws FileNotFoundException
+   * @throws IOException
+   */
+  public String getPlainTextContent(String path) throws FileNotFoundException, IOException {
+    String text;
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+      StringBuilder builder = new StringBuilder();
+      String line = reader.readLine();
+
+      while (line != null) {
+        builder.append(line);
+        builder.append(System.lineSeparator());
+
+        line = reader.readLine();
+      }
+
+      text = builder.toString();
+    }
+
+    return text;
+  }
+
+  /**
+   * 
+   * @param path
+   * @return
+   */
+  public String getDocxTextContent(String path) {
+    String docText = "";
+
+    try {
+      FileInputStream fis = new FileInputStream(path);
+      XWPFDocument xdoc = new XWPFDocument(OPCPackage.open(fis));
+      XWPFWordExtractor extractor = new XWPFWordExtractor(xdoc);
+
+      docText = extractor.getText();
+      
+      extractor.close();
+    } catch(Exception ex) {
+      ex.printStackTrace();
+    }
+
+    return docText;
+  }
+
+  /**
+   * 
+   * @param path
+   * @return
+   */
+  public String getDocTextContent(String path) {
+    String docText = "";
+
+    try {
+      File file = new File(path);
+      FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+
+      HWPFDocument doc = new HWPFDocument(fis);
+      WordExtractor we = new WordExtractor(doc);
+      String[] paragraphs = we.getParagraphText();
+
+      for (String para : paragraphs)
+        docText += para.toString();
+
+      fis.close();
+      we.close();
+    } catch (Exception e) {
+      //logger.error(e);
+      e.printStackTrace();
+    }
+
+    return docText;
+  }
+
+  /**
+   * 
+   * @param path
+   * @return
+   * @throws IOException 
+   */
+  public String getPDFContent(String path) throws IOException {
+    String content = "";
+    PDDocument document = PDDocument.load(new File(path));
+
+    if (!document.isEncrypted()) {
+      PDFTextStripper stripper = new PDFTextStripper();
+      String text = stripper.getText(document);
+
+      content = text;
+    }
+
+    document.close();
+    return content;
+  }
+}
--- src/main/java/com/kebabwarriors/fileindexer/Procedures.java
+++ src/main/java/com/kebabwarriors/fileindexer/Procedures.java
@@ -1,11 +1,11 @@
-package com.kebabwarriors.file_indexer;
+package com.kebabwarriors.fileindexer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-public final class MathProcedures {
+public final class Procedures {
   /**
    * @param doc  list of strings
    * @param term String represents a term
@@ -14,11 +14,9 @@ public final class MathProcedures {
   public static double tf(String[] doc, String term) { 
     double result = 0; 
 
-    for (String word : doc) { 
-      if (term.equalsIgnoreCase(word)) {
-        result++;
-      }
-    } 
+    for (String word : doc)
+      if (term.equalsIgnoreCase(word))
+        result += 1;
 
     return (result / doc.length); 
   }
@@ -31,15 +29,13 @@ public final class MathProcedures {
   private static double idf(HashMap<String, String[]> docs, String term) { 
     double total = 0; 
 
-    for (String[] words : docs.values()) {
-      for (String word : words) {
+    for (String[] words : docs.values())
+      for (String word : words)
         if (term.equals(word)) { 
-          total++; 
+          total += 1; 
 
           break;   
         }
-      }
-    }
     
     return idfValidator(docs.size(), total); 
   }
@@ -65,15 +61,14 @@ public final class MathProcedures {
     Double normalVectorOne = 0.0;
     Double normalVectorTwo = 0.0;
     
-    if (vectorOneList.size() > vectorTwoList.size()) {
+    if (vectorOneList.size() > vectorTwoList.size())
       while (vectorOneList.size() != vectorTwoList.size())
         vectorTwoList.add((double) 0);
-    } else {
+    else
       while (vectorOneList.size() != vectorTwoList.size())
         vectorOneList.add((double) 0);
-    }
     
-    for (int x = 0; x < vectorOne.length; x++) {
+    for (int x = 0; x < vectorOne.length; x += 1) {
       dotProduct = vectorOneList.get(x) * vectorTwoList.get(x);
       normalVectorOne += Math.pow(vectorOneList.get(x), 2);
       normalVectorTwo += Math.pow(vectorTwoList.get(x), 2);
